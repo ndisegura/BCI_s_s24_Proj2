a263897d7566d5b7ac713fd2e6be296e529d3e0f
Apc-03 <118464923+Apc-03@users.noreply.github.com>
2024-04-08 10:53:16 -0400

    Update test_predict_ssvep_data.py
    PartE and fixed format issues

--- test_predict_ssvep_data.py
+++ test_predict_ssvep_data.py
@@ -28,49 +28,49 @@ plt.close('all')
 # Build data file string
 data_directory = './SsvepData/'
 subject = 1
-#%%
-# PartA: Generate Predictions
 
+# PartA: Generate Predictions
 # Load subject data
 data = imp.load_ssvep_data(subject, data_directory)
 
 # Epoch subject data
-epoch_start_time =0
-epoch_end_time =20
-#eeg_epochs, epoch_times, is_trial_15Hz = imp.epoch_ssvep_data(data,epoch_start_time,epoch_end_time)
+epoch_start_time = 0
+epoch_end_time = 20
+# eeg_epochs, epoch_times, is_trial_15Hz = imp.epoch_ssvep_data(data,epoch_start_time,epoch_end_time)
 
 # # Get epoched fft data
 # fs = data['fs']
 # eeg_epochs_fft, fft_frequencies = imp.get_frequency_spectrum(eeg_epochs, fs)
 
-# Generate predicted labels
-#Changed function argument so that we can reuse this function in part C. Also added "channel" argument. Need to fix functin to handle multiple channel. Using just one for now
-predicted_labels = prd.generate_predicted_labels(data,epoch_start_time,epoch_end_time, 'Oz') 
+# Generate predicted labels Changed function argument so that we can reuse this function in part C. Also added
+# "channel" argument. Need to fix functin to handle multiple channel. Using just one for now
+predicted_labels = prd.generate_predicted_labels(data, epoch_start_time, epoch_end_time, 'Oz')
 
 print(predicted_labels)
 
-#%%
 # PartB: Calculate Accuracy and ITR
 
-event_types=data['event_types']
+event_types = data['event_types']
 
-accuracy,ITR=prd.get_accuracy_ITR(data,event_types,predicted_labels)
-print(accuracy)
-print(ITR)
+accuracy, ITR = prd.get_accuracy_ITR(data, event_types, predicted_labels)
+# print(accuracy)
+# print(ITR)
 
-#%%
 # PartC: Loop Through Epoch Limits
-accuracy_matrix,ITR_matrix,loop_epoch_time=prd.loop_epoch_limits(data, epoch_start_time_limit=5, epoch_end_time_limit=20, step=2.0, channel='Oz') 
-print(accuracy_matrix)
-print(ITR_matrix)
+accuracy_matrix, ITR_matrix, loop_epoch_time = prd.loop_epoch_limits(data, epoch_start_time_limit=5,
+                                                                     epoch_end_time_limit
+                                                                     =20, step=2.0, channel='Oz')
+# print(accuracy_matrix)
+# print(ITR_matrix)
 
-#%%
 # PartD: Plot results
+prd.generate_pseudocolor_plots(accuracy_matrix, ITR_matrix, loop_epoch_time)
 
-prd.generate_pseudocolor_plots(accuracy_matrix,ITR_matrix,loop_epoch_time)
-
-#%%
 # PartE: Create Predictor Histogram
+epoch_start_time = 7.5
+epoch_end_time = 17
+channel = 'Oz'
+prd.plot_predictor_histogram(data, epoch_start_time, epoch_end_time, channel)
 
 
 
